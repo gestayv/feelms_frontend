cd8b8fbbac8eef3d4dede3357b550d9d9df5dd38
luzanto <pablo.caceres.l@usach.cl>
2017-06-05 05:04:05 -0400

    Admin fixed

--- js/controllers.js
+++ js/controllers.js
@@ -69,6 +69,148 @@ app.controller("searchController", function($scope, $state) {
     }
 });
 
+app.controller("adminViewController", function($scope, $state) {
+    $scope.showAdminView = function() {
+        $state.go("admin");
+    }
+});
+
+app.controller("filmsController", function($scope, $http, GETService) {
+  function GET_Peliculas(GETService) {
+    GETService.GET_Peliculas()
+    .then(function(respuesta) {
+      $scope.data_peliculas = respuesta.data;
+      console.log(respuesta.data);
+    }, function(error){console.log(error)});
+  }
+  GET_Peliculas(GETService);
+});
+
+app.controller('editFormController', function($scope, $http) {
+  $scope.pelicula = {};
+  $scope.master = {};
+
+  $scope.showInfo = function(pelicula) {
+    var generos = pelicula.genres;
+    var director = pelicula.director.id.toString();
+    var keyterms = pelicula.keyterms;
+
+    if (generos.length >= 1) {  // Se verifica si es que posee generos o no (caso improbable, pero de suceder se podría todo)
+      var genre = generos[0].id;
+      for (var i = 1; i < generos.length; i++) {
+        genre =  genre + "," + generos[i].id;
+      }
+    }
+
+    if(keyterms.length >= 1) {  // Se verifica si es que posee keywords o no (caso improbable, pero de suceder se podría todo)
+      var kt = keyterms[0].term;
+      for (var i = keyterms.length - 1; i >= 1; i--) {
+        kt = kt + ',' + keyterms[i].term;
+      };
+    }
+    delete pelicula['keyterms'];
+    console.log(pelicula);
+    $scope.pelicula = angular.copy(pelicula);
+    $scope.pelicula.genres = angular.copy(genre);
+    $scope.pelicula.director = angular.copy(director);
+    $scope.pelicula.keywords = angular.copy(kt);
+  };
+
+  $scope.edit = function(pelicula) {
+    if (pelicula.genres != null) {
+      var to_list = pelicula.genres;
+      to_list = to_list.split(",");
+      pelicula.genres = to_list;
+    }
+    if (pelicula.keywords != null) {
+      var to_list = pelicula.keywords;
+      to_list = to_list.split(",");
+      pelicula.keywords = to_list;
+    }
+    pelicula.length = parseInt(pelicula.length);
+    pelicula.admin = parseInt(pelicula.admin);
+    pelicula.director = parseInt(pelicula.director);
+    pelicula.imgId = parseInt(pelicula.imgId);
+
+     var wat = JSON.stringify(pelicula);
+     console.log(wat);
+     var req = {
+     method: 'POST',
+     url: 'http://131.221.33.124:8080/feelms/api/admin/update/film',
+     headers: {
+       'Content-Type': 'application/json'
+     },
+     data: wat
+    }
+    $http(req).then(function(response){
+      console.log(response);
+    }, function(response){
+      console.log(response);
+    });
+  };
+});
+
+app.controller('addFormController', function($scope, $http) {
+  $scope.master = {};
+  $scope.update = function(pelicula) {
+    var to_list = pelicula.genres;
+    to_list = to_list.split(",");
+    for (var i = 0; i < to_list.length; i++) {
+      to_list[i] = parseInt(to_list[i]);
+    }
+    pelicula.genres = to_list;
+    pelicula.length = parseInt(pelicula.length);
+    pelicula.admin = parseInt(pelicula.admin);
+    pelicula.director = parseInt(pelicula.director);
+    pelicula.imgId = parseInt(pelicula.imgId);
+    var to_list = pelicula.keywords;
+    to_list = to_list.split(",");
+    pelicula.keywords = to_list;
+    var wat = JSON.stringify(pelicula);
+    console.log(wat);
+    var req = {
+     method: 'POST',
+     url: 'http://131.221.33.124:8080/feelms/api/admin/add/film',
+     headers: {
+       'Content-Type': 'application/json'
+     },
+     data: wat
+    }
+    $http(req).then(function(response){
+      console.log(response);
+    }, function(response){
+      console.log(response);
+    });
+  };
+
+  $scope.reset = function() {
+    $scope.pelicula = angular.copy($scope.master);
+  };
+
+  $scope.list_genre = [];
+  //$scope.text_genre = 'hello';
+  $scope.submitgenres = function() {
+      if(angular.isArray($scope.pelicula.genre) == false)
+      {
+       $scope.pelicula.genre = [];
+      }
+      $scope.pelicula.genre.push(parseInt(this.text_genre));
+      $scope.list_genre.push(parseInt(this.text_genre));
+      $scope.text_genre = '';
+  };
+
+  $scope.submitkeywords = function() {
+      if(angular.isArray($scope.pelicula.keywords) == false)
+      {
+       $scope.pelicula.keywords = [];
+      }
+      $scope.pelicula.keywords.push(parseInt(this.text_keywords));
+      $scope.list_genre.push(parseInt(this.text_keywords));
+      $scope.text_keywords = '';
+  };
+  //$scope.reset();
+});
+
 app.controller("filmController", function($scope, $stateParams, $http, GETService) {
 
     function GET_Info_Pelicula(id_pelicula){
@@ -117,6 +259,7 @@ app.controller('topController', function($scope, $http, GETService) {
     //	Que el ranking que se muestra en home sea anual.
     GET_TOP_Peliculas(10,365);
 });
+
 app.controller('genresController',function($scope,$http,GETService){
 
     function GET_TOP_Genres(cant_genres, cant_dias){
@@ -146,6 +289,7 @@ app.controller('genresController',function($scope,$http,GETService){
     GET_TOP_Genres(10, 30);
 
 });
+
 app.directive('headerSection', function() {
     return {
         restrict: 'E',
